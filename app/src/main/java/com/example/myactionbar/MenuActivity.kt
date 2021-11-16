fe19311fc9e6fe21a30ffddc145e2723cdec6675
iputungurahkertawijaya <putungurah8833@gmail.com>
2021-11-16 20:56:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myactionbar/MenuActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myactionbar.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
